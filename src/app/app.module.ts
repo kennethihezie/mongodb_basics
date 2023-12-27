72fcb55a6688a4e95e76868ec2d2fc586af95233
kennethihezie <kennethihezie06@gmail.com>
2023-12-27 16:07:04 +0100

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { Helpers } from 'src/utils/helpers';
 import { AccountModule } from 'src/account/account.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(Helpers.databaseUrl, { dbName: 'users_db' }), UserModule, AccountModule],
+  imports: [MongooseModule.forRoot(Helpers.databaseUrl, { dbName: 'users_db'}), UserModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
